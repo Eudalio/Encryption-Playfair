7ac4461e78b3c26c06325a729df0ddd019a61706
eudalio <tonnydallios@gmail.com>
2018-03-12 15:36:06 -0300

    Quebrando mensagem em pares e separando as repeticoes de silaba - pronto

--- playfair.js
+++ playfair.js
@@ -2,11 +2,12 @@ window.onload = function(){
     // Variáveis globais
     let inputKey  = document.getElementById('key');
     let inputMessage  = document.getElementById('message');
-
+    let grid = [];    
+    
     document.getElementById("gerarkey").addEventListener('click', function(event){
         event.preventDefault();
-        key = inputKey.value.toUpperCase().trim().replace('J','I').replace(/[^A-Z]/g, '');
-        //console.log(key);
+        let key = inputKey.value.toUpperCase().trim().replace('J','I').replace(/[^A-Z]/g, '');
+        console.log(key);
 
         // CREATE GRID FROM KEY
         const alphabet = ['ABCDEFGHIKLMNOPQRSTUVWXYZ'];
@@ -14,7 +15,6 @@ window.onload = function(){
         const keyGrid = [...new Set(`${key}${alphabet}`)];
         console.log(keyGrid);
     
-        let grid = [];    
         for(let i = 0; i < keyGrid.length; i += 5){
             grid.push(keyGrid.slice(i, (i+5)));
         }
@@ -24,7 +24,23 @@ window.onload = function(){
     document.getElementById("encrypt").addEventListener('click', function(event){
         event.preventDefault();
                
-        console.log(inputMessage.value);
+        const message = inputMessage.value.toUpperCase().replace(/[^A-Z]/g, '').replace(/J/g, 'I').split('').filter(x => x !== ' ');
+        //console.log(message);
+        const duples = [];
+        for(let i = 0; i < message.length; i += 2){
+            const dupleCurrent = message.slice(i, i + 2);
+            if(dupleCurrent.length !== 2){
+                dupleCurrent.push('X');
+                duples.push(dupleCurrent);
+            } else if(dupleCurrent[0] === dupleCurrent[1]){
+                message.splice(i+1,0,'X');
+                duples.push(message.slice(i, i + 2));
+            } else{
+                duples.push(dupleCurrent);
+            }
+        }
+
+        //console.log(duples);
         
     });
 
